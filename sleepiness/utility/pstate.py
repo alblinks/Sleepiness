fba0113bc25764c9ae5755e029c2bff8193e8d1b
nikpau <n.paulig@web.de>
2024-03-15 14:15:04 +0100

    add passenger state enum

---
+++ sleepiness/utility/pstate.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+class PassengerState(Enum):
+    """
+    Enum class for the state of the passenger
+    and the corresponding labels.
+    
+    We define the following states in alphabetical order:
+    """
+    AWAKE = 0
+    NOTTHERE = 1
+    SLEEPING = 2
+    
+    def __str__(self):
+        return self.name
+
+class ReducedPassengerState(Enum):
+    """
+    Reduced Enum class for the state 
+    of the passenger to only test 
+    for sleeping or awake.
+    
+    This is used as the classification
+    for empty or occupied seats is relatively
+    trivial, which is why we can reduce the
+    number of states to two.
+    
+    We define the following states in alphabetical order:
+    """
+    AWAKE = 0
+    SLEEPING = 1
+    
+    def __str__(self):
+        return self.name
