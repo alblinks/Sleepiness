c8729dd907cca86a78ec7ec8562c2946a11fd787
nikpau <n.paulig@web.de>
2024-04-16 11:53:21 +0200

    real time classification using label aggregation

--- sleepiness/utility/realTimeDetection.py
+++ sleepiness/utility/realTimeDetection.py
@@ -1,39 +1,76 @@
+from collections import deque
 import cv2
-from sleepiness.pipelines import NoEyePipeline
+import sleepiness.pipelines as pipelines
+from sleepiness.test.aggregators import LabelAggregator, MajorityVoting
 
-# Load trained model
-#model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/exp2/weights/last.pt', force_reload=True)
+def framewise_real_time_detection(model: pipelines.Pipeline) -> None:
+    """
+    Real time classification for every frame from the camera.
+    """
+    # Access camera
+    cap = cv2.VideoCapture("/dev/video0")
 
-# Classify img
-#img = os.path.join('data', 'images', 'awake.8a08edee-ca53-11ee-9dae-6bc3a5752140.jpg')
-#results = model(img)
+    while cap.isOpened():
 
-# Display it
-#plt.imshow(np.squeeze(results.render()))
-#plt.show()
-#plt.savefig("Example.pdf")
+        # Read current frame
+        ret, frame = cap.read()
+        assert ret, "Error reading frame"
+        # Make detections 
+        result = model.classify(frame)
+        
+        # Write a text in the top right corner
+        cv2.putText(frame, f"{result}", (10, 10), cv2.QT_FONT_NORMAL, 0.5, (0, 255, 0), 2)
+        # Render
+        cv2.imshow(f"Real-Time Detection", frame)
+        
+        # Exit
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            return
 
-model = NoEyePipeline()
+def aggregated_real_time_detection(model: pipelines.Pipeline, 
+                                   aggregator: LabelAggregator) -> None:
+    """
+    Real time classification for every frame from the camera.
+    """
+    # Access camera
+    cap = cv2.VideoCapture("/dev/video0")
+    while cap.isOpened():
 
-# Access camera
-cap = cv2.VideoCapture("/dev/video0")
+        # Read current frame
+        ret, frame = cap.read()
+        assert ret, "Error reading frame"
+        # Make detections 
+        result = model.classify(frame)
+        aggregator.add(result)
+        
+        # Write the aggregated result in the top right corner
+        cv2.putText(frame, f"{aggregator.state}", 
+                    (10, 10), cv2.QT_FONT_NORMAL, 
+                    0.5, (150, 25, 160), 1
+                )
 
-while cap.isOpened():
+        # Write the individual results in the bottom right corner
+        for i, state in enumerate(aggregator.labels):
+            cv2.putText(frame, f"{state}", 
+                        (10, 10 + 20 * (i + 1)), cv2.QT_FONT_NORMAL, 
+                        0.5, (0, 180, 60), 1)
 
-    # Read current frame
-    ret, frame = cap.read()
-    # Make detections 
-    result = model.classify(frame)
-    
-    # Write a text in the top right corner
-    cv2.putText(frame, f"{result}", (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    # Render
-    cv2.imshow(f"Detection", frame)
-    
-    
-    # Exit
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+        # Render
+        cv2.imshow(f"Real-Time Detection", frame)
+        
+        # Exit
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            return
 
+if __name__ == "__main__":
+    model = pipelines.FullPipeline(
+        eye_model_confidence=0.2 ,
+        hand_model_confidence=0.5,
+    )
+    #framewise_real_time_detection(model)
+    aggregator = MajorityVoting()
+    aggregated_real_time_detection(model, aggregator)
