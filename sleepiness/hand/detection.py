b0b37cd5562b692eb1dc27b50aaea8c93f6f445e
nikpau <n.paulig@web.de>
2024-04-29 12:38:45 +0200

    swap hand model to mediapipe

--- sleepiness/hand/detection.py
+++ sleepiness/hand/detection.py
@@ -1,19 +1,34 @@
-from pathlib import Path
-from sleepiness.hand.handYolo import HandYOLO
-from sleepiness import __path__ as p
+"""
+Hand Landmarks detection using google's mediapipe library
+"""
+import cv2
+import mediapipe as mp
+import numpy as np
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from sleepiness.hand.detection import __path__ as assetPath
 
-def load_model(confidence: float = 0.2) -> HandYOLO:
-    """Loads and returns the hand model."""
+base_options = python.BaseOptions(model_asset_path=f'{assetPath[0]}/hand_landmarker.task')
+options = vision.HandLandmarkerOptions(
+    base_options=base_options,
+    num_hands=2,min_hand_detection_confidence = 0.15,
+    min_hand_presence_confidence = 0.15,
+    min_tracking_confidence = 0.15
+)
 
-    try:
-        cfg_path     = Path(p[0]) / "hand" / "cross-hands.cfg"
-        weights_path = Path(p[0]) / "hand" / "cross-hands.weights"
-        hand_model = HandYOLO(cfg_path, weights_path, ["hand"])
-    except:
-        raise FileNotFoundError("Error: Could not load the hand model. Check the paths.")
-    
-    hand_model.size = 416
-    hand_model.confidence = confidence
+detector = vision.HandLandmarker.create_from_options(options)
 
-    print("Hand model loaded.")
-    return hand_model
+def draw_landmarks_on_image(image, result):
+    annotated_image = image.copy()
+    for hand_landmarks in result.hand_landmarks:
+        for l1, l2 in zip(hand_landmarks,hand_landmarks[1:]):
+            cv2.line(annotated_image, (int(l1.x*image.shape[1]), int(l1.y*image.shape[0])),
+                     (int(l2.x*image.shape[1]), int(l2.y*image.shape[0])), (57, 17, 122), 2)
+    return annotated_image
+
+def detect_from_numpy_array(image: np.ndarray):
+    image = image.astype(np.uint8)
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+
+    res = detector.detect(image)
+    return res if res.hand_landmarks else None
--- sleepiness/hand/realTimeHandDetection.py
+++ sleepiness/hand/realTimeHandDetection.py
@@ -1,11 +1,8 @@
 import cv2
-from sleepiness.hand import load_model
+from sleepiness.hand.detection import detect_from_numpy_array, draw_landmarks_on_image
 
 
 if __name__ == "__main__":
-
-    # Load hand model
-    hand_model = load_model()
 
     # Access camera
     cap = cv2.VideoCapture("/dev/video0")
@@ -16,18 +13,10 @@ if __name__ == "__main__":
         ret, frame = cap.read()
 
         # Inference 
-        width, height, inference_time, results = hand_model.inference(frame)
-
-        # Display hands
-        for detection in results:
-            id, name, confidence, x, y, w, h = detection
-
-            # draw a bounding box rectangle and label on the image
-            color = (0, 255, 255)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-            text = "%s (%s)" % (name, round(confidence, 2))
-            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, color, 2)
+        detection_result = detect_from_numpy_array(frame)
+        if detection_result:
+            annotated_image = draw_landmarks_on_image(frame, detection_result)
+            frame = annotated_image
         
         # Display the image
         cv2.imshow("Hand Detection", frame)
