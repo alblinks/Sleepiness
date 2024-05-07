b0e59643b59d393ddd7a143c809e136c6314c02e
nikpau <n.paulig@web.de>
2024-05-07 09:39:49 +0200

    release

--- sleepiness/__init__.py
+++ sleepiness/__init__.py
@@ -1,9 +1,9 @@
 from sleepiness.utility.logger import logger
 from sleepiness.utility.pstate import PassengerState, ReducedPassengerState
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 __greeting__ = f"""
--------------Welcome to---------------------
+---------------Welcome to-------------------
  __ _                 _                     
 / _\ | ___  ___ _ __ (_)_ __   ___  ___ ___ 
 \ \| |/ _ \/ _ \ '_ \| | '_ \ / _ \/ __/ __|
