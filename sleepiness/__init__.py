901b72553434c0567d4f5ba1e100411520e67489
nikpau <n.paulig@web.de>
2024-05-16 16:23:33 +0200

    remove greeting

--- sleepiness/__init__.py
+++ sleepiness/__init__.py
@@ -2,14 +2,3 @@ from sleepiness.utility.logger import logger
 from sleepiness.utility.pstate import PassengerState, ReducedPassengerState
 
 __version__ = "1.0.2"
-__greeting__ = f"""
----------------Welcome to-------------------
- __ _                 _                     
-/ _\ | ___  ___ _ __ (_)_ __   ___  ___ ___ 
-\ \| |/ _ \/ _ \ '_ \| | '_ \ / _ \/ __/ __|
-_\ \ |  __/  __/ |_) | | | | |  __/\__ \__ \\
-\__/_|\___|\___| .__/|_|_| |_|\___||___/___/
-               |_|                          
-------------Version: {__version__}-----------------
-"""
-print(__greeting__)
